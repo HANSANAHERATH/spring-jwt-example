5be14de35a49ce6a9161209fd10287cb32b3ebb6
Hansana Herath <subodhahansana@gmail.com>
2021-10-15 21:49:10 +0530

    initial project

---
+++ src/test/java/com/jwt/example/springjwtexample/SpringJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.example.springjwtexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
